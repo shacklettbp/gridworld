4999e6514e96e4438d8d584c0ff38624af689326
Brennan Shacklett <bpshacklett@gmail.com>
2023-07-15 23:53:20 -0700

    Update lstm test script

--- scripts/lstm_test.py
+++ scripts/lstm_test.py
@@ -3,7 +3,7 @@ import madrona_learn
 torch.manual_seed(0)
 
 num_channels = 2
-batch_size = 1
+batch_size = 2
 seq_len = 8
 
 fast_lstm = madrona_learn.rnn.FastLSTM(
@@ -11,7 +11,6 @@ fast_lstm = madrona_learn.rnn.FastLSTM(
     hidden_channels = num_channels * 2,
     num_layers = 1,
 )
-fast_hidden = torch.zeros(*fast_lstm.hidden_shape[0:2], batch_size, fast_lstm.hidden_shape[2], dtype=torch.float32)
 
 slow_lstm = madrona_learn.rnn.LSTM(
     in_channels = num_channels,
@@ -23,14 +22,14 @@ with torch.no_grad():
     for dst, src in zip(slow_lstm.lstm.parameters(), fast_lstm.rnn.parameters()):
         dst.copy_(src)
 
-slow_hidden = torch.zeros(*slow_lstm.hidden_shape[0:2], batch_size, slow_lstm.hidden_shape[2], dtype=torch.float32)
+hidden_start = torch.randn(*slow_lstm.hidden_shape[0:2], batch_size, slow_lstm.hidden_shape[2], dtype=torch.float32)
 
 inputs = torch.randn(seq_len, batch_size, num_channels)
 
 def single_test():
     print("SINGLE TEST\n")
-    fast_out, new_fast_hidden = fast_lstm(inputs[0], fast_hidden)
-    slow_out, new_slow_hidden = slow_lstm(inputs[0], slow_hidden)
+    fast_out, new_fast_hidden = fast_lstm(inputs[0], hidden_start)
+    slow_out, new_slow_hidden = slow_lstm(inputs[0], hidden_start)
 
     print(fast_out)
     print(slow_out)
@@ -40,13 +39,14 @@ def single_test():
 single_test()
 
 print("\n\nSEQUENCE TEST\n")
-sequence_breaks = torch.tensor([0, 0, 0, 1, 0, 0, 1, 0], dtype=torch.bool)
+sequence_breaks = torch.tensor([0, 0, 0, 1, 0, 1, 0, 0], dtype=torch.bool)
+print("seq breaks: ", sequence_breaks.to(dtype=torch.uint8))
 sequence_breaks = sequence_breaks.view(seq_len, 1, 1).expand(seq_len, batch_size, 1).contiguous()
 print(sequence_breaks.shape)
 
-fast_outputs = fast_lstm.fwd_sequence(inputs, fast_hidden, sequence_breaks)
+fast_outputs = fast_lstm.fwd_sequence(inputs, hidden_start, sequence_breaks)
 print(fast_outputs)
 
-slow_outputs = slow_lstm.fwd_sequence(inputs, slow_hidden, sequence_breaks)
+slow_outputs = slow_lstm.fwd_sequence(inputs, hidden_start, sequence_breaks)
 print(slow_outputs)
 
