420cb429677e53a9c1443a944a062696199f5ce3
VSAnimator <sarukkai@stanford.edu>
2023-11-13 11:30:19 -0800

    Go-explore working for tabular worlds

--- scripts/tabular_world.py
+++ scripts/tabular_world.py
@@ -31,26 +31,32 @@ class TabularWorld():
         self.transitions = torch.tensor(transitions, device = device)
         self.transition_rewards = torch.tensor(rewards, device = device)
         # Current state
-        self.observations = torch.zeros((num_worlds, 1), dtype=torch.int32, device = device)
-        self.actions = torch.zeros((num_worlds, 1), dtype=torch.int32, device = device)
-        self.dones = torch.zeros((num_worlds, 1), dtype=torch.int32, device = device)
+        self.observations = torch.zeros((num_worlds, 1), dtype=torch.int64, device = device)
+        self.actions = torch.zeros((num_worlds, 1), dtype=torch.int64, device = device)
+        self.dones = torch.zeros((num_worlds, 1), dtype=torch.int64, device = device)
         self.rewards = torch.zeros((num_worlds, 1), device = device)
         # Flag for reset per world
-        self.force_reset = torch.zeros(num_worlds, dtype=torch.int32, device = device)
+        # self.force_reset = torch.zeros(num_worlds, dtype=torch.int32, device = device)
 
     def step(self):
+        #print("Input Observations in call", self.observations[:1])
         # Apply force_reset where needed
-        self.observations[self.force_reset] = 0
-        self.force_reset[...] = 0
+        #print("Any force_reset?", self.force_reset[0])
+        #self.observations[self.force_reset] = 0
+        #self.force_reset[...] = 0
         # Assume self.actions has been set, index into transition matrix to get next state and reward
-        #print("Actions", self.actions)
-        #print("Observations", self.observations)
+        #print("Step Actions", self.actions[:10])
+        #print("Pre-Step Observations in call", self.observations[:1])
         #print(self.transition_rewards[self.observations])
         #print(self.transition_rewards[self.observations, self.actions])
-        self.rewards[...] = self.transition_rewards[self.observations,self.actions]
-        self.observations[...] = self.transitions[self.observations,self.actions]
+        self.rewards[:,0] = self.transition_rewards[self.observations[:,0],self.actions[:,0]]
+        self.observations[:,0] = self.transitions[self.observations[:,0],self.actions[:,0]]
         self.dones[...] = (self.observations == self.transitions.shape[0] - 1).int()
         # Reset all the dones
         #print(self.observations.shape)
-        #print(self.dones.shape)
+        #print("Step rewards", self.rewards[:10])
+        #print("Post-Step observations in call", self.observations[:1])
+        #print("Step dones", self.dones[:10])
+        #print("Next transitions", self.transitions[self.observations[:10],:])
         self.observations[self.dones == 1] = 0
+        #print("Reset observations", self.observations[:10])
